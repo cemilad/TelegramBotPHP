7d6868d6af67410435dd8d1d49247d1426d26b79
cemilad <51002687+cemilad@users.noreply.github.com>
2019-05-26 00:56:03 +0430

    Create mybot.php

---
+++ mybot.php
@@ -0,0 +1,17 @@
+include("Telegram.php");
+date_default_timezone_set("asia/tehran");
+// Set the bot TOKEN
+$bot_id = "700160045:AAExUKX8-Ja9695rU3yxGWXl9-qf7BuTtTQ";
+// Instances the class
+$telegram = new Telegram($bot_id);
+$text = $telegram->Text(); // 
+$username = $telegram->Username(); // 
+$name = $telegram->FirstName();
+$family = $telegram->LastName();
+$message_id = $telegram->MessageID(); //
+$user_id = $telegram->UserID(); //
+$chat_id = $telegram->ChatID(); //
+if(!is_null($text) && !is_null($chat_id)){
+$content = array('chat_id' => $chat_id, 'text' => $user_id);
+$telegram->sendMessage($content);
+}
